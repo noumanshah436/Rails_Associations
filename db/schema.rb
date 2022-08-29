b20c055a1c4c120d595acf03ae0088ff7c2b40e8
noumanshah436 <noumanshah436@gmail.com>
2022-08-29 15:13:17 +0500

    create course model for has_and_belongs_to_many Association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_081526) do
+ActiveRecord::Schema.define(version: 2022_08_29_101135) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2022_08_29_081526) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["student_id"], name: "index_blogs_on_student_id"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
