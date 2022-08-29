c816467617599bbda5768e3e30f92cf104974c2c
noumanshah436 <noumanshah436@gmail.com>
2022-08-29 14:36:25 +0500

    seeding student data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# create single student
+# student = Student.create( name: "Nouman", email: "nouman@gmail.com")
+
+# create 20 student
+20.times do |i|
+  puts "creating Student#{i+1}"
+  Student.create(
+    name: "Student#{i+1}",
+    email: "student#{i+1}@gmail.com")
+end
